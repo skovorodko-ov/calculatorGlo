e86e9087be1e003675d60836854d137da3dedf61
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-08-01 16:18:33 +0300

    add lesson02

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 2500;
+let income = 'фриланс';
+let addExpenses = 'Такси, камуналка, телефон, интернет';
+let deposit = true;
+let mission = 1000000;
+let period = 6;
+let budgetDay = money / 30;
 
-alert('Hello world!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof true);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
 
-console.log('Hello world!');
+addExpenses = addExpenses.toLocaleLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+console.log('budgetDay: ', budgetDay);
