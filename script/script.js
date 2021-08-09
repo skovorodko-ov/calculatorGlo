587be9a52e4b87ca6b72ac51de0fceae66a3f95c
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-08-09 19:17:57 +0300

    added lesson09

--- script/script.js
+++ script/script.js
@@ -4,8 +4,30 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let money,
-    start = function() {
+const btnStart = document.getElementById('start');
+const btnIncomeAdd = document.getElementsByTagName('button')[0];
+const btnExpensesAdd = document.getElementsByTagName('button')[1];
+const checkBox = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+
+
+let money;
+let start = function() {
       do {
         money = prompt('Ваш месячный доход?', 50000);
       }
@@ -128,3 +150,4 @@ for (let key in appData) {
 console.log(appData.addExpenses.map(function (item) {
   return (item.charAt(0).toUpperCase() + item.slice(1));
 }).join(', '));
+
